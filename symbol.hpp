ed160512a4b4f8289eb156acea8b5418cb728535
SpirosPapa <135608838+SpirosPapa@users.noreply.github.com>
2024-08-04 18:55:26 +0300

    initial  symbol.hpp

--- symbol.hpp
+++ symbol.hpp
@@ -3,4 +3,8 @@
 
 enum Datatype { DATATYPE_int, DATATYPE_byte };
 
+enum Types { Type_int, Type_byte, Type_bool , Type_int_array , Type_byte_array }
+
+enum Rtypes {  Proc , rtype_int , rtype_byte }
+
 #endif
